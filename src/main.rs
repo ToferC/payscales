83f96e97ca7eecd0b4ad3761540af9135f68e191
toferc <cgeist7@gmail.com>
2020-05-07 14:05:07 -0400

    heroku setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+
+#[get("/again")]
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello World again")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(index2)
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
