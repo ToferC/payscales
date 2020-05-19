09bcbafd1774ff51072c39464fe126173c130dba
toferc <cgeist7@gmail.com>
2020-05-19 11:00:04 -0400

    add info to index page

--- src/handlers/handlers.rs
+++ src/handlers/handlers.rs
@@ -10,7 +10,41 @@ use actix_web::{web, get, HttpResponse, Responder};
 
 #[get("/")]
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello World")
+    HttpResponse::Ok().body(r#"
+    <h1>Welcome to GC-PayScales</h1>
+    <p>This application is an API for Government of Canada pay/salary scales for classifications and levels. The intent is to have a central, easy to access and use resource for accurate pay information. This will reduce duplication and potential errors across organizations and serve as an example of how APIs can support day-to-day business in government.</p>
+    
+    <p>This is also a learning project in Rust and my first foray into GraphQL development.</p>
+    
+    <p>You can explore and experiment with the API here: <a href="https://gc-payscales.herokuapp.com/playground">https://gc-payscales.herokuapp.com/playground</a></p>
+    
+    <p>A sample query looks like this:
+    {
+        group(identifier:	EC) {
+          payscaleForLevel(level: 3) {
+            name
+            level
+            steps
+            currentRatesOfPay{
+              salary(step: 3)
+            }
+            rateOfPaysForDate(date: "2020-06-23"){
+              inForce
+              salary(step: 3)
+            }
+          }
+        }
+      }</p>
+
+    <p>Please note that this work is a learning project, may contain errors and should not be used to make pay-related decisions.</p>
+    <p>The PayScraper project is available on GitHub under an MIT licence here: <a href="https://github.com/ToferC/payscraper">https://github.com/ToferC/payscraper</a></p>
+    <p>The API is available here under the same licence: <a href="https://github.com/ToferC/payscales">https://github.com/ToferC/payscales</a></p>
+
+    <p>Developed by ToferC 2020</p>
+
+
+    
+    "#)
 }
 
 #[get("/api")]
